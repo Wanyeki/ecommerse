13037e024365d3dbe82c708258a3f4c1219fe1c6
wanyeki <wanyekifs@gmail.com>
2019-10-13 14:26:38 +0300

    added navbar drop-downs

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,6 +1,7 @@
 import '../styles/navbar.css';
 import React, { Component } from 'react';
-
+import image from '../images/avatar.jpg'
+import image2 from '../images/z.jpg'
 class Navbar extends Component {
     state = { 
         collapsed:true,
@@ -39,15 +40,67 @@ class Navbar extends Component {
         </div>
         <div className="nav-right">
             <ul className="nav-links animated slideInDown">
-                <li className="nav-link"><i class="fa fa-comment nav-icon has-pill"><div className="pill">{this.state.db_data.messages}</div></i></li>
+                <li className="nav-link"><i class="fa fa-comment nav-icon has-pill"><div className="pill">{this.state.db_data.messages}
+                </div></i>
+                <div className="nav-drop">
+                    <div className="drop-content">
+                        <div className="nav-drop-pointer"></div>
+                    <div className="message-screen">
+                        <span>Messages</span>
+                        <div className="msgs">
+                       <div class="recieved"> <img src={image} alt="" className="msg-avatar"/> <div className="message">Your item have 10 more views.</div></div>
+                       <div class="recieved"> <img src={image} alt="" className="msg-avatar"/> <div className="message">Wanyeki asks you to join pro version </div></div>
+                       <div class="recieved"> <img src={image} alt="" className="msg-avatar"/> <div className="message">Wanyeki asks you to join pro version </div></div>
+                       <div class="recieved"> <img src={image} alt="" className="msg-avatar"/> <div className="message">Lorem ipsum dolor sit amet consectetur adipisicing elit.</div></div>
+                        
+                      </div>
+                      
+                      
+                    </div>
+                    </div>
+                </div>
+                </li>
                 <li className="nav-link"><i class="fa fa-heart nav-icon has-pill"><div className="pill">{this.state.db_data.favourite}</div></i></li>
                 <li className="nav-link"><i class="fa fa-plus nav-icon has-pill"></i></li>
-                <li className="nav-link"><i class="fa fa-shopping-cart nav-icon has-pill"><div className="pill">{this.state.db_data.messages}</div></i></li>
+                <li className="nav-link"><i class="fa fa-shopping-cart nav-icon has-pill"><div className="pill">{this.state.db_data.messages}</div></i>
+                <div className="cart">
+                    <div className="cart-items">
+                    <div className="nav-drop-pointer"></div>
+                        <div className="cart-content">
+                            <span>cart</span>
+                            <div className="cart-item">
+                                <div className="cart-item-image"><img src={image2} alt="" className="cart-item-image"/></div>
+                                <div className="cart-item-name">Frying pan</div>
+                                <div className="cart-item-price">Ksh 150</div>
+                              <i class="fa fa-trash" aria-hidden="true"></i>
+                            </div>
+                            <div className="cart-item">
+                                <div className="cart-item-image"><img src={image2} alt="" className="cart-item-image"/></div>
+                                <div className="cart-item-name">Frying pan</div>
+                                <div className="cart-item-price">Ksh 150</div>
+                              <i class="fa fa-trash" aria-hidden="true"></i>
+                            </div>
+                            <div className="cart-item">
+                                <div className="cart-item-image"><img src={image2} alt="" className="cart-item-image"/></div>
+                                <div className="cart-item-name">Frying pan</div>
+                                <div className="cart-item-price">Ksh 150</div>
+                              <i class="fa fa-trash" aria-hidden="true"></i>
+                            </div>
+                            <div className="cart-item">
+                                <div className="cart-item-image"><img src={image2} alt="" className="cart-item-image"/></div>
+                                <div className="cart-item-name">Frying pan</div>
+                                <div className="cart-item-price">Ksh 150</div>
+                              <i class="fa fa-trash" aria-hidden="true"></i>
+                            </div>
+                            <button>Send interested messages</button>
+                        </div>
+                    </div>
+                </div></li>
                 <li className="nav-link"><i class="fa fa-question nav-icon has-pill"></i></li>
                 <li className="nav-link nav-link-active">Sign up</li>
 
-            </ul>
-        </div>
+            </ul>  
+        </div> 
     </div>
 </div>
 
