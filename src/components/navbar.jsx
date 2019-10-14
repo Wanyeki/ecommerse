f4066ad26dead4e36fb9c3d6ae8799243557d9d7
wanyeki <wanyekifs@gmail.com>
2019-10-14 09:37:14 +0300

    added navbar dropdowns

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -8,11 +8,21 @@ class Navbar extends Component {
         db_data:{
             messages:2,
             favourite:1
-        }
+        },
+        messages_panel:'none',
+        cart_panel:'none'
+
 
 
     }
-
+messages_toggleHandler=()=>{
+ this.setState({messages_panel:'block',
+                cart_panel:'none'});
+}
+cart_toggleHandler=()=>{
+ this.setState({cart_panel:'block',
+                messages_panel:'none'});
+}
     toggleHandler=()=>{
         if(this.state.collapsed){
              document.querySelector('.nav-links').style.display='none';
@@ -40,9 +50,9 @@ class Navbar extends Component {
         </div>
         <div className="nav-right">
             <ul className="nav-links animated slideInDown">
-                <li className="nav-link"><i class="fa fa-comment nav-icon has-pill"><div className="pill">{this.state.db_data.messages}
+                <li className="nav-link" onClick={this.messages_toggleHandler}><i class="fa fa-comment nav-icon has-pill"><div className="pill">{this.state.db_data.messages}
                 </div></i>
-                <div className="nav-drop">
+                <div className="nav-drop animated slideInUp" style={{display:this.state.messages_panel}}>
                     <div className="drop-content">
                         <div className="nav-drop-pointer"></div>
                     <div className="message-screen">
@@ -62,37 +72,50 @@ class Navbar extends Component {
                 </li>
                 <li className="nav-link"><i class="fa fa-heart nav-icon has-pill"><div className="pill">{this.state.db_data.favourite}</div></i></li>
                 <li className="nav-link"><i class="fa fa-plus nav-icon has-pill"></i></li>
-                <li className="nav-link"><i class="fa fa-shopping-cart nav-icon has-pill"><div className="pill">{this.state.db_data.messages}</div></i>
-                <div className="cart">
+                <li className="nav-link" onClick={this.cart_toggleHandler}><i class="fa fa-shopping-cart nav-icon has-pill"><div className="pill">{this.state.db_data.messages}</div></i>
+                <div className="cart " style={{display:this.state.cart_panel}}>
                     <div className="cart-items">
-                    <div className="nav-drop-pointer"></div>
                         <div className="cart-content">
                             <span>cart</span>
                             <div className="cart-item">
+                               <div className="number">1.</div>
                                 <div className="cart-item-image"><img src={image2} alt="" className="cart-item-image"/></div>
                                 <div className="cart-item-name">Frying pan</div>
                                 <div className="cart-item-price">Ksh 150</div>
                               <i class="fa fa-trash" aria-hidden="true"></i>
                             </div>
                             <div className="cart-item">
+                               <div className="number">1.</div>
                                 <div className="cart-item-image"><img src={image2} alt="" className="cart-item-image"/></div>
                                 <div className="cart-item-name">Frying pan</div>
                                 <div className="cart-item-price">Ksh 150</div>
                               <i class="fa fa-trash" aria-hidden="true"></i>
                             </div>
                             <div className="cart-item">
+                               <div className="number">1.</div>
                                 <div className="cart-item-image"><img src={image2} alt="" className="cart-item-image"/></div>
                                 <div className="cart-item-name">Frying pan</div>
                                 <div className="cart-item-price">Ksh 150</div>
                               <i class="fa fa-trash" aria-hidden="true"></i>
                             </div>
                             <div className="cart-item">
+                               <div className="number">1.</div>
                                 <div className="cart-item-image"><img src={image2} alt="" className="cart-item-image"/></div>
                                 <div className="cart-item-name">Frying pan</div>
                                 <div className="cart-item-price">Ksh 150</div>
                               <i class="fa fa-trash" aria-hidden="true"></i>
                             </div>
-                            <button>Send interested messages</button>
+
+                            <div className="cart-item">
+                               <div className="number">1.</div>
+                                <div className="cart-item-image"><img src={image2} alt="" className="cart-item-image"/></div>
+                                <div className="cart-item-name">Frying pan</div>
+                                <div className="cart-item-price">Ksh 150</div>
+                              <i class="fa fa-trash" aria-hidden="true"></i>
+                            </div>
+
+                            
+                            <button class="btn-p">Send interested messages</button>
                         </div>
                     </div>
                 </div></li>
@@ -109,4 +132,4 @@ class Navbar extends Component {
     }
 }
  
-export default Navbar;
+export default Navbar; 
